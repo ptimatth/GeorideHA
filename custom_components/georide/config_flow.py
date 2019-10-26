94f97e4917740fc4d68263c28f89ee99f1eb9a27
Matthieu <matthieu@duval-dev.fr>
2019-10-26 17:18:07 +0200

    Update flow

--- custom_components/georide/config_flow.py
+++ custom_components/georide/config_flow.py
@@ -1,14 +1,16 @@
+""" Georide config flow file """
+
 from homeassistant import config_entries
+import voluptuous as vol
+
 from .const import DOMAIN
 
 class GeorideConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Geride config flow """
     async def async_step_user(self, info):
-        if info is not None:
-            # process info
-
         return self.async_show_form(
             step_id='user',
             data_schema=vol.Schema({
-              vol.Required('password'): str
-            })
-        )
+                vol.Required('password'): str
+            }))
+        

38ceb3a2a16a798b769ac505a0494a93055790d3
djodjo02130 <11382082+djodjo02130@users.noreply.github.com>
2025-04-19 22:35:52 +0000

    Add Georide 3S

--- custom_components/georide/device.py
+++ custom_components/georide/device.py
@@ -52,6 +52,8 @@ class Device:
         elif self._tracker.version == 3:
             if self._tracker.model == 'georide-3':
                 name = "GeoRide 3"
+            else if self._tracker.model == 'georide-3s':
+                name = "GeoRide 3S"
             else:
                 name = "GeoRide Mini"
         else:
